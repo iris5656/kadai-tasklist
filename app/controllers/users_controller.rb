d1c1bbae0d95cd9d4b7c3efea8aedc50349864ff
saki.tudura <techiris5656@yahoo.co.jp>
2019-07-15 13:37:03 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  #before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
 
   def create
     # 1. form作成
-
     # 3. 実際にユーザーを作成するプログラムをcreateに記載する
     @user = User.new(user_params)
     if @user.save
